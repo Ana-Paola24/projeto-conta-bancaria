861f6481012234b0c3fea2a6058d1c5ad2e93a72
nome <you@example.com>
2024-04-29 22:51:31 -0300

    adição dos atributos na conta.model

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,12 +1,20 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);   //atributos
+		c1.visualizar();
+		c1.sacar(12000.0f); //verificação de saldo suficiente
+		c1.visualizar();
+		c1.depositar(5000.0f); //adição de valor
+		c1.visualizar();
+		
 		
 		Scanner leia = new Scanner (System.in);
 		
